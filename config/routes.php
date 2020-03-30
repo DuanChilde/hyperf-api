9ac5e5bcc6506d094306fc065cf9e6cb18cf5208
august <2971749820@qq.com>
2020-03-30 15:12:49 +0800

    事件机制demo

--- config/routes.php
+++ config/routes.php
@@ -13,3 +13,20 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+
+//公共路由
+Router::addGroup(
+'/common/',
+    function(){
+        require_once __DIR__.'/route/common.php';
+    }
+);
+
+//api v1 路由
+Router::addGroup(
+    '/api/v1/',
+    function (){
+        require_once __DIR__.'/route/api/v1.php';
+    }
+);
+
