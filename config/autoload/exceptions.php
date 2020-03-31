f7c42077e2395785b1aeaad622c60a977d12a4fb
august <2971749820@qq.com>
2020-03-31 10:05:32 +0800

    验证器异常处理

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 return [
     'handler' => [
         'http' => [
+            App\Exception\Handler\ValidationExceptionHandler::class,
             App\Exception\Handler\AppExceptionHandler::class,
         ],
     ],
